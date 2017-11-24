185a22fc387b903d01aef69bd671c405807a7651
曾云 <zengyun@ziyitong.com.cn>
2017-11-24 15:08:05 +0800

    1.添加转MP3格式 2.播放本地、网络音频

--- AudiorecorderDemo/AudiorecorderDemo/AudioRecorderEngine.h
+++ AudiorecorderDemo/AudiorecorderDemo/AudioRecorderEngine.h
@@ -13,44 +13,55 @@
 #import <MediaPlayer/MediaPlayer.h>
 
 typedef void(^AudioRecorderEngineSoundWaveBlock) (float progress);
-typedef void(^AudioRecorderEngineFinishBlock) ();
+typedef void(^AudioRecorderEngineFinishBlock) (void);
+typedef void(^AudioRecorderEngineChangMP3FinishBlock) (void);
+typedef void (^AudioRecorderEngineAudioPlayFinishBlock)(void);
+typedef void (^AudioRecorderEngineAudioPlayProgressBlock)(float progress);
 
+#define AudioRecorder [AudioRecorderEngine sharedAudioRecorderEngine]
 @interface AudioRecorderEngine : NSObject
 
 #pragma mark -- 录制音频对象
-@property (nonatomic,readonly)NSString *audioSavePath;/**< 存放录音的地址 */
+@property (nonatomic,readonly)NSString *audioSavePath;/**< 存放录音的地址caf */
+@property (nonatomic,readonly)NSString *audioMP3Path;/**< 转换成MP3的地址 需要添加lame库*/
 @property (nonatomic,readonly)NSData *audioData;/**< 录音文件大小 */
 @property (nonatomic,readonly)AVAudioRecorder *audioRecorder;/**< 录音播放对象 */
 @property (assign, nonatomic,readonly) long long audioDurationTime;/**< 录制时长 */
+@property (nonatomic,assign) BOOL isChangeMP3;/**< 默认是转换  转换成功 会删掉源文件*/
 
 
 #pragma mark -- 播放音频对象
 @property (nonatomic,strong) NSString *networkAudioPath;/**< 网络文件地址 */
 @property (nonatomic,strong) NSString *audioPath;/**< 本地文件地址 */
 @property (nonatomic,assign) BOOL isPlayNetwork;/**< 默认播放录制本地  YES 播放网络音频  */
+@property (nonatomic,assign) double audioPlayProgress;/**< 语音播放进度 */
 
 
 
+
+@property (nonatomic,copy)AudioRecorderEngineAudioPlayFinishBlock audioPlayFinishBlock;/**< 播放完成回调 */
+@property (nonatomic,copy)AudioRecorderEngineAudioPlayProgressBlock audioPlayProgressBlock ;/**< 播放进度 */
 @property (nonatomic,copy)AudioRecorderEngineSoundWaveBlock soundWaveBlock;/**< 返回声波的大小 */
 @property (nonatomic,copy)AudioRecorderEngineFinishBlock finishBlcok;/**< 录制完成时候 */
+@property (nonatomic,copy)AudioRecorderEngineChangMP3FinishBlock changMP3FinishBlcok;/**< 录制完成时候 */
 
 + (AudioRecorderEngine *)sharedAudioRecorderEngine;
 
 /**
- 这个方法是为了给自定义存储位置准备的   
+ 这个方法是为了给自定义存储位置准备的
  如果需要自定义的话  就必须先实现这个方法
  如果等AudioRecorder对象创建完成时候，不允许修改的
-
+ 
  @param filePath document 后面的文件路径   最后是以Audio.caf结尾
  */
 - (void)setAudioSaveFilePath:(NSString *)filePath;/**< 自定义路径 存储录音文件 */
 
 /**
- 删除录制 或者其他本地的录音
+ 删除录制本地的录音
  */
 - (void)removeAudio;
 
-
+- (void)removeAudioMP3;
 
 - (void)audioRecorderPlay;/**< 开始录音 */
 - (void)audioRecorderStop;/**< 结束录音 */
--- AudiorecorderDemo/AudiorecorderDemo/AudioRecorderEngine.m
+++ AudiorecorderDemo/AudiorecorderDemo/AudioRecorderEngine.m
@@ -8,8 +8,10 @@
 
 #import "AudioRecorderEngine.h"
 
+#import "lame.h"
 
 #define kRecordAudioFile @"Audio.caf"
+#define kRecordAudioMP3File @"Audio.mp3"
 @interface AudioRecorderEngine ()<AVAudioRecorderDelegate>
 
 @property (nonatomic,strong) AVAudioRecorder *audioRecorder;/**< 录音对象 */
@@ -17,6 +19,7 @@
 @property (nonatomic,strong) AVPlayer *avPlayer;/**< 播放器对象  用于播放音频 */
 @property (nonatomic,strong) AVPlayerItem * playerItem;
 @property (nonatomic,strong) NSTimer *timer;/**< 定时器，用于监听录音声波的大小 */
+@property (nonatomic,strong) NSTimer *playTimer;/**< 定时器，用于监听播放时间的 */
 
 @property (nonatomic,strong) NSString *filePath;/**< 传进来的文件的名字 */
 
@@ -26,11 +29,13 @@
 
 + (AudioRecorderEngine *)sharedAudioRecorderEngine {
     static AudioRecorderEngine *sharedAudioRecorderEngineInstance = nil;
-        static dispatch_once_t pred;
-        dispatch_once(&pred, ^{
-            sharedAudioRecorderEngineInstance = [[AudioRecorderEngine alloc]init];
-            NSLog(@"sharedAudioRecorderEngineInstance");
-        });
+    static dispatch_once_t pred;
+    dispatch_once(&pred, ^{
+        sharedAudioRecorderEngineInstance = [[AudioRecorderEngine alloc]init];
+        sharedAudioRecorderEngineInstance.isChangeMP3 = YES;
+        sharedAudioRecorderEngineInstance.audioPlayProgress = 0.0f;
+        NSLog(@"sharedAudioRecorderEngineInstance");
+    });
     return sharedAudioRecorderEngineInstance;
 }
 
@@ -38,19 +43,13 @@
     _filePath = filePath;
 }
 
-
 - (void)removeAudio {
     NSString *path = [self.audioSavePath substringToIndex:(self.audioSavePath.length - kRecordAudioFile.length -1)];
     NSLog(@"%@",path);
     if (![[NSFileManager defaultManager] fileExistsAtPath:path]) {
         return;
     }
-    NSEnumerator *childFilesEnumerator = [[[NSFileManager defaultManager] subpathsAtPath:path] objectEnumerator];
-    
-    NSString* fileName;
-   while ((fileName = [childFilesEnumerator nextObject]) != nil){
-            [self removeFile:fileName path:path];
-        }
+    [PathEngine removeFile:kRecordAudioFile path:path];
 }
 
 //删除文件
@@ -65,6 +64,19 @@
     return ret;
 }
 
+- (void)removeAudioMP3 {
+    if (_audioMP3Path.length == 0) {
+        return;
+    }
+    NSString *path = [self.audioMP3Path substringToIndex:(self.audioMP3Path.length - kRecordAudioMP3File.length -1)];
+    NSLog(@"%@",path);
+    if (![[NSFileManager defaultManager] fileExistsAtPath:path]) {
+        return;
+    }
+    [PathEngine removeFile:kRecordAudioMP3File path:path];
+    
+}
+
 
 - (AVAudioRecorder *)audioRecorder {
     if (!_audioRecorder) {
@@ -72,11 +84,11 @@
         NSURL *url = [NSURL fileURLWithPath:[self setSavePath]];
         //2.设置录音的配置
         NSDictionary *soundSetting = [NSDictionary dictionaryWithObjectsAndKeys:
-                        [NSNumber numberWithFloat:8000.0f],AVSampleRateKey,
-                        [NSNumber numberWithInt:2],AVNumberOfChannelsKey,
-                        [NSNumber numberWithInt:AVAudioQualityHigh],AVEncoderAudioQualityKey,
-                        [NSNumber numberWithInt:16],AVEncoderBitRateKey,
-                        nil];
+                                      [NSNumber numberWithFloat:8000.0f],AVSampleRateKey,
+                                      [NSNumber numberWithInt:2],AVNumberOfChannelsKey,
+                                      [NSNumber numberWithInt:AVAudioQualityHigh],AVEncoderAudioQualityKey,
+                                      [NSNumber numberWithInt:16],AVEncoderBitRateKey,
+                                      nil];
         /*
          1.AVNumberOfChannelsKey 通道数 通常为双声道 值2
          2.AVSampleRateKey 采样率 单位HZ 通常设置成44100 也就是44.1k
@@ -126,40 +138,40 @@
     [self.audioRecorder updateMeters];//更新测量值
     float power= [self.audioRecorder averagePowerForChannel:0];//取得第一个通道的音频，注意音频强度范围时-160到0
     CGFloat progress= (1.0/160.0)*(power+160.0);
-//    NSLog(@"power ===== %f progress==== %f",power,progress);    
+    //    NSLog(@"power ===== %f progress==== %f",power,progress);
     if (self.soundWaveBlock) {
         self.soundWaveBlock (progress);
     }
 }
 
 - (NSString *)setSavePath {
-        NSString *path=[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    NSString *path=[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
     
     if (self.filePath.length >0) {
-      path =   [path stringByAppendingPathComponent:[NSString stringWithFormat:@"/%@", self.filePath]];
+        path =   [path stringByAppendingPathComponent:[NSString stringWithFormat:@"/%@", self.filePath]];
     }
-        NSFileManager *fileManager = [NSFileManager defaultManager];
-        if(![fileManager fileExistsAtPath:path]) //如果不存在
-    
-        {
-            NSMutableString *tmpPath = [[NSMutableString alloc] initWithCapacity:1];
-            NSArray *dirs = [path componentsSeparatedByString:@"/"];
-            [dirs enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-                if ( ![obj isEqualToString:@""] ) {
-                    [tmpPath appendFormat:@"/%@", obj];
-                    if ( ![fileManager fileExistsAtPath:tmpPath] ) {
-                        [fileManager createDirectoryAtPath:tmpPath withIntermediateDirectories:YES attributes:nil error:nil];
-                    }
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if(![fileManager fileExistsAtPath:path]) //如果不存在
+        
+    {
+        NSMutableString *tmpPath = [[NSMutableString alloc] initWithCapacity:1];
+        NSArray *dirs = [path componentsSeparatedByString:@"/"];
+        [dirs enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            if ( ![obj isEqualToString:@""] ) {
+                [tmpPath appendFormat:@"/%@", obj];
+                if ( ![fileManager fileExistsAtPath:tmpPath] ) {
+                    [fileManager createDirectoryAtPath:tmpPath withIntermediateDirectories:YES attributes:nil error:nil];
                 }
-            }];
-
-        }else{
-            NSLog(@"xxx.txt is  exist");
-            
-        }
+            }
+        }];
+        
+    }else{
+        NSLog(@"xxx.txt is  exist");
+        
+    }
     path=[path stringByAppendingPathComponent:kRecordAudioFile];
     
-        NSLog(@"file path:%@",path);
+    NSLog(@"file path:%@",path);
     _audioSavePath = path;
     
     return path;
@@ -176,16 +188,20 @@
  */
 -(void)audioRecorderDidFinishRecording:(AVAudioRecorder *)recorder successfully:(BOOL)flag{
     
-//    NSLog(@"%f",recorder.currentTime);
-//    NSLog(@"%f",recorder.deviceCurrentTime);
+    //    NSLog(@"%f",recorder.currentTime);
+    //    NSLog(@"%f",recorder.deviceCurrentTime);
     AVURLAsset* audioAsset =[AVURLAsset URLAssetWithURL:[NSURL fileURLWithPath:self.audioSavePath] options:nil];
     CMTime audioDuration = audioAsset.duration;
     _audioDurationTime = CMTimeGetSeconds(audioDuration);
-//    NSLog(@"audioDurationSeconds========%lld",self.audioDurationTime);
-//    NSLog(@"audioDurationSeconds>>========%f",ceilf(self.audioDurationTime));
+    //    NSLog(@"audioDurationSeconds========%lld",self.audioDurationTime);
+    //    NSLog(@"audioDurationSeconds>>========%f",ceilf(self.audioDurationTime));
     NSLog(@"%lu",[NSData dataWithContentsOfFile:self.audioSavePath].length);
     _audioData = [NSData dataWithContentsOfFile:self.audioSavePath];
     _audioPath = self.audioSavePath;
+    
+    if (self.isChangeMP3) {
+        [self audioToMP3:_audioPath];
+    }
     NSLog(@"录音完成!");
     if (self.finishBlcok) {
         self.finishBlcok();
@@ -193,6 +209,66 @@
     
 }
 
+- (void)audioToMP3:(NSString *)audioPath {
+    
+    if ([PathEngine isExistFile:audioPath]) {
+        // 输出路径
+        NSString *outPath = [[audioPath stringByDeletingPathExtension] stringByAppendingString:@".mp3"];
+        
+        
+        @try {
+            size_t read, write;
+            
+            FILE *pcm = fopen([audioPath cStringUsingEncoding:1], "rb");  //source 被转换的音频文件位置
+            fseek(pcm, 4*1024, SEEK_CUR);                                   //skip file header
+            FILE *mp3 = fopen([outPath cStringUsingEncoding:1], "wb");  //output 输出生成的Mp3文件位置
+            
+            const int PCM_SIZE = 8192;
+            const int MP3_SIZE = 8192;
+            short int pcm_buffer[PCM_SIZE*2];
+            unsigned char mp3_buffer[MP3_SIZE];
+            
+            lame_t lame = lame_init();
+            lame_set_num_channels(lame, 2);//设置1为单通道，默认为2双通道
+            lame_set_in_samplerate(lame, 8000.0);//11025.0
+            //lame_set_VBR(lame, vbr_default);
+            lame_set_brate(lame, 16);
+            lame_set_mode(lame, 3);
+            lame_set_quality(lame, 2); /* 2=high 5 = medium 7=low 音质*/
+            lame_init_params(lame);
+            
+            do{
+                read = fread(pcm_buffer, 2*sizeof(short int), PCM_SIZE, pcm);
+                if (read == 0) {
+                    write = lame_encode_flush(lame, mp3_buffer, MP3_SIZE);
+                }else{
+                    write = lame_encode_buffer_interleaved(lame, pcm_buffer, (int)read, mp3_buffer, MP3_SIZE);
+                }
+                
+                fwrite(mp3_buffer, write, 1, mp3);
+                
+                
+            }while (read!=0);
+            
+            lame_close(lame);
+            fclose(mp3);
+            fclose(pcm);
+        }
+        @catch (NSException *exception) {
+            NSLog(@"%@",[exception description]);
+        }
+        @finally {
+            
+            [self removeAudio];
+            _audioMP3Path = outPath;
+            
+            NSLog(@"MP3生成成功%lu",[NSData dataWithContentsOfFile:outPath].length);
+            if (self.changMP3FinishBlcok) {
+                self.changMP3FinishBlcok();
+            }
+        }
+    }
+}
 
 /**
  停止录音
@@ -266,6 +342,14 @@
 }
 
 
+- (NSTimer *)playTimer {
+    if (!_playTimer) {
+        _playTimer = [NSTimer scheduledTimerWithTimeInterval:0.1f target:self selector:@selector(audioPlayTime) userInfo:nil repeats:YES];
+    }
+    return _playTimer;
+}
+
+
 #pragma mark -- 设置AVPlayerItem 对象
 - (void)setPlayerItem:(AVPlayerItem *)playerItem {
     if (_playerItem) {
@@ -282,14 +366,28 @@
                                                  name:AVPlayerItemDidPlayToEndTimeNotification
      
                                                object:self.playerItem];
+    
+}
+
+- (void)audioPlayTime {
+    //    NSLog(@"audioPlayTime === %f  %f  %f",CMTimeGetSeconds(self.playerItem.currentTime),CMTimeGetSeconds(self.playerItem.duration), CMTimeGetSeconds(self.playerItem.currentTime) / CMTimeGetSeconds(self.playerItem.duration));
+    double progress = CMTimeGetSeconds(self.playerItem.currentTime) / CMTimeGetSeconds(self.playerItem.duration);
+    if (progress) {
+        self.audioPlayProgress = progress;
+        if (self.audioPlayProgressBlock) {
+            self.audioPlayProgressBlock(progress);
+        }
+    }
 }
 
 #pragma mark -- 播放结束监听
 - (void)playerItemDidReachEnd {
     NSLog(@"playerItemDidReachEnd 音乐播放完成...");
+    if (self.audioPlayFinishBlock) {
+        self.audioPlayFinishBlock();
+    }
     [self audioStop];
 }
-
 
 #pragma mark -- 监听播放状态
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
@@ -299,6 +397,8 @@
         AVPlayerStatus status= [[change objectForKey:@"new"] intValue];
         if(status == AVPlayerStatusReadyToPlay){
             NSLog(@"开始播放,总长度:%.2f",CMTimeGetSeconds(playerItem.duration));
+            self.playTimer.fireDate = [NSDate distantPast];
+            //            self.timer.fireDate=[NSDate distantPast];
         }else if(status == AVPlayerStatusUnknown){
             NSLog(@"%@",@"AVPlayerStatusUnknown");
             [self playerItemDidReachEnd];
@@ -318,9 +418,12 @@
     //1.如果
     
     //如果你播放的是网络音频，请传入一个Url 使用[NSURL URLWithString:self.networkAudioPath]
-    NSURL * url = [NSURL fileURLWithPath:self.audioPath];
+    NSURL * url;
     if (self.isPlayNetwork) {
-        url = [NSURL URLWithString:self.networkAudioPath];
+        //        url = [NSURL URLWithString:self.networkAudioPath];
+        url = [NSURL URLWithString: [self.networkAudioPath stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+    } else {
+        url = [NSURL fileURLWithPath:self.audioPath];
     }
     AVPlayerItem * item = [AVPlayerItem playerItemWithURL:url];
     self.playerItem = item;
@@ -335,6 +438,10 @@
     _avPlayer = nil;
     _audioPlayer = nil;
     _playerItem = nil;
+    _networkAudioPath = kMNullStr;
+    _audioPath = kMNullStr;
+    [self.playTimer invalidate];
+    self.playTimer = nil;
 }
 
 
